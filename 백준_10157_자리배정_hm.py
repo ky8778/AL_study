d4250aecc86cf568587f21abb4f2b4dd846f36c9
pyohamen <hmpyo1101@gmail.com>
2020-02-10 02:33:59 +0900

    런타임 에러

---
+++ 백준_10157_자리배정_hm.py
@@ -0,0 +1,51 @@
+# def isSafy (y,x):
+#     if y >=0 and y<=R and x >=0 and y <=C:
+#         return True
+
+import sys
+sys.stdin = open('백준_10157_자리배정.txt','r')
+
+C, R = map(int, input().split()) # 7,6
+tl = int(input())
+
+zero = [[0 for _ in range(C)] for __ in range(R)]
+
+cnt = 0
+
+x=0 #6
+y=R-1 #5
+
+while cnt != tl:
+    while zero[y-1][x] != 1:
+        zero[y][x] = 1
+        cnt += 1
+        if cnt == tl:
+            print('{} {}'.format(x+1,6-y))
+            break
+        y -= 1
+
+
+    for i in range(C-1):
+        zero[y][x] = 1
+        cnt +=1
+        if cnt == tl:
+            print('{} {}'.format(x+1,6-y))
+            break
+        x += 1
+    x += 1
+    for i in range(R-1):
+        zero[y][x] = 1
+        cnt +=1
+        if cnt == tl:
+            print('{} {}'.format(x+1,6-y))
+            break
+        y += 1
+
+    while zero[y][x-1] != 1:
+        zero[y][x] = 1
+        cnt += 1
+        if cnt == tl:
+            print('{} {}'.format(x+1,6-y))
+            break
+        x -= 1
+    break
