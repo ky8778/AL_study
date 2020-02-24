237f6022dd1bb74b9c6dbf8b596f2faa30868287
leeyeeun <yr0901@naver.com>
2020-02-24 22:39:48 +0900

    ye

--- A/BruteForce/BJ2231분해합_YE.py
+++ A/BruteForce/BJ2231분해합_YE.py
@@ -1,44 +1,21 @@
 #분해합
-N=int(input())
-nlist = list(map(int,str(N)))
+N = int(input())
 lenN=len(str(N))
-alist=[]
-anw= True
-n=lenN-1
+
+temp = N - 9*lenN
+if temp < 0:
+    temp = 0
 
 while True:
-    if N <=18 and n == 0:
-        if N%2==0:
-            alist.append(N//2)
-            break
-        else:
-            anw=False
-            break
-    else:
-        if n == lenN - 1:
-            a = nlist[lenN-1-n]
-        else:
-            a = 9
-        while True:
-            R = N-(10**n +1)*a
-            if R<0:
-                a-=1
-            else:
-                temp=0
-                for t in range(n):
-                    temp += 9*(10**t +1)
-                if R<=temp:
-                    a-=1
-                else:
-                    alist.append(a+1)
-                    N -= (10**n +1)*(a+1)
-                    n = n-1
-                    break
-            
-if anw:
-    alist=map(str, alist)
-    anw = ''.join(alist)
-    print(anw.lstrip('0'))
-    #print(anw)
-else:
-    print("0")
+    tsum=temp
+    for i in str(temp):
+        tsum+=int(i)
+    
+    if temp > N:
+        print (0)
+        break
+    elif tsum != N:
+        temp += 1
+    elif tsum == N:
+        print(temp)
+        break
