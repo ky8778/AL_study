705c44e9dcb8cfabfe1d67c0aa5e32ebb0386071
Tae-jung <taehee7590@naver.com>
2020-03-05 19:00:47 +0900

    딕셔너리추가

--- A/DFS_BFS/BJ1260DFS와BFS_TJ.py
+++ A/DFS_BFS/BJ1260DFS와BFS_TJ.py
@@ -40,3 +40,45 @@ for i in range(M):
 
 print(*DFS(graph, V))
 print(*BFS(graph, V))
+
+# ## 딕셔너리
+# def DFS(graph, start):
+#     visited = []
+#     stack = []
+
+#     stack.append(start)
+
+#     while stack:
+#         node = stack.pop()
+#         if node not in visited:
+#             visited.append(node)
+#             stack.extend(graph[node])
+    
+#     return visited
+
+
+# def BFS(graph, start):
+#     visited = []
+#     queue = []
+
+#     queue.append(start)
+
+#     while queue:
+#         node = queue.pop(0)
+#         if node not in visited:
+#             visited.append(node)
+#             queue.extend(graph[node])
+    
+#     return visited
+
+
+# N, M, V = map(int,input().split())
+# edge = [list(map(int,input().split())) for _ in range(M)]
+# graph = {n : [] for n in range(1, N + 1)}
+# for i in range(M):
+#     graph.get(edge[i][0]).append(edge[i][1])
+#     graph.get(edge[i][1]).append(edge[i][0])
+    
+    
+# print(*DFS(graph, V))
+# print(*BFS(graph, V))
